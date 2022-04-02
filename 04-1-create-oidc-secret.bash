2466532c6f547a017cf2bdf2439d443bfd4a7e58
seubpong.mon <pjame.fb@gmail.com>
2022-04-02 14:25:27 +0700

    Created OIDC secret

--- 04-1-create-oidc-secret.bash
+++ 04-1-create-oidc-secret.bash
@@ -8,8 +8,8 @@ NS=airflow
 cat << EOF | kubectl apply -n ${NS} -f -
 apiVersion: v1
 data:
-  GOOGLE_OAUTH2_CLIENT_ID: $(echo -n ${OIDC_CLIENT_ID} | base64)
-  GOOGLE_OAUTH2_SECRET: $(echo -n ${OIDC_CLIENT_SECRET} | base64)
+  GOOGLE_OAUTH2_CLIENT_ID: "$(echo -n ${OIDC_CLIENT_ID} | base64)"
+  GOOGLE_OAUTH2_SECRET: "$(echo -n ${OIDC_CLIENT_SECRET} | base64)"
 kind: Secret
 metadata:
   name: ${SECRET}
