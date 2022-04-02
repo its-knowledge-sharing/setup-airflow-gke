5c897d679b98f6c91d67f1216c056d9e83a4a455
seubpong.mon <pjame.fb@gmail.com>
2022-04-02 11:01:15 +0700

    Initial commit

---
+++ 03-1-deploy-airflow.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source .env
+
+NS=airflow
+kubectl create ns ${NS}
+
+
+helm repo add airflow https://airflow-helm.github.io/charts
+
+helm template airflow airflow/airflow \
+    -f airflows/airflow-1-basic.yaml \
+    --version 8.5.3 \
+    --namespace ${NS} > tmp-external-secrets.yaml
+
+kubectl apply -n ${NS} -f tmp-external-secrets.yaml
