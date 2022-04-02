9a50095b5d77503bf4a668813091b48ae48fbf74
seubpong.mon <pjame.fb@gmail.com>
2022-04-02 12:03:53 +0700

    Added scripts

--- 02-2-create-dns-zone.bash
+++ 02-2-create-dns-zone.bash
@@ -3,4 +3,6 @@
 source .env
 
 # Create DNS managed zone
-gcloud dns managed-zones create ${ZONE_NAME} --dns-name=${DNS_NAME} --project ${PROJECT}
+gcloud dns managed-zones create ${ZONE_NAME} --dns-name=${DNS_NAME} \
+    --description "Zone for airflow" \
+    --project ${PROJECT}
