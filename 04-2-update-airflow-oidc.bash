7e9c1dbf2e1b1a26044f7e2406f90bd74ed4b7d5
seubpong.mon <pjame.fb@gmail.com>
2022-04-02 14:39:11 +0700

    Update airflow to use OIDC

---
+++ 04-2-update-airflow-oidc.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source .env
+
+NS=airflow
+kubectl create ns ${NS}
+
+
+helm repo add airflow https://airflow-helm.github.io/charts
+
+helm template airflow airflow/airflow \
+    -f airflows/airflow-3-google-oidc.yaml \
+    --version 8.5.3 \
+    --namespace ${NS} > tmp-airflow.yaml
+
+kubectl apply -n ${NS} -f tmp-airflow.yaml
