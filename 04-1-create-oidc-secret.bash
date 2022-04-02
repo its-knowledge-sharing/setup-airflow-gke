40cad57d2906bca123b59fa4a2657d66b4b46b43
seubpong.mon <pjame.fb@gmail.com>
2022-04-02 14:32:16 +0700

    Created OIDC secret

--- 04-1-create-oidc-secret.bash
+++ 04-1-create-oidc-secret.bash
@@ -9,8 +9,8 @@ TMP=secret.tmp
 cat << EOF > ${TMP}
 apiVersion: v1
 data:
-  GOOGLE_OAUTH2_CLIENT_ID: "$(echo -n ${OIDC_CLIENT_ID} | base64)"
-  GOOGLE_OAUTH2_SECRET: "$(echo -n ${OIDC_CLIENT_SECRET} | base64)"
+  GOOGLE_OAUTH2_CLIENT_ID: "$(echo -n ${OIDC_CLIENT_ID} | base64 -w0)"
+  GOOGLE_OAUTH2_SECRET: "$(echo -n ${OIDC_CLIENT_SECRET} | base64 -w0)"
 kind: Secret
 metadata:
   name: ${SECRET}
